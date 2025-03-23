962de6918b65029ee2deeed856dee89752f90470
deityyGH <paprdovich@gmail.com>
2025-03-23 20:04:20 +0100

    before DI

--- SGTC/Models/ICoilCalculator.cs
+++ SGTC/Models/ICoilCalculator.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SGTC.Models
 {
     public interface ICoilCalculator
     {
+        void CalculatePrimary(double? _turns = null, double? _coreDiameter = null, double? _wireDiameter = null, double? _wireInsDiameter = null, double? _spacing = null, double? _capacitance = null);
 
     }
 }
